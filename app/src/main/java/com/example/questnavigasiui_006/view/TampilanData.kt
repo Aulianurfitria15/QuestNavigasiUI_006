399141ceb81a267c0e79d5e4370b028568b905e6
Aulianurfitria15 <aulianurfitria17@gmail.com>
2025-11-07 16:19:05 +0700

    membuat tampilan Scaffold baru di halaman TampilanData,

--- app/src/main/java/com/example/questnavigasiui_006/view/TampilanData.kt
+++ app/src/main/java/com/example/questnavigasiui_006/view/TampilanData.kt
@@ -35,3 +35,10 @@ fun TampilanData(
         Pair(stringResource(id = R.string.jenis_kelamin), "Lainnya"),
                 Pair(stringResource(id = R.string.alamat), "Yogyakarta")
     )
+    Scaffold (modifier = Modifier,
+        {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.tampil), color = Color.White) },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(colorResource(id = R.color.teal_700))
+            )
+        }){ isiRuang ->
