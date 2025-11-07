fa4ff35649849b45dce46c4f14f4a5acb5c5689f
Aulianurfitria15 <aulianurfitria17@gmail.com>
2025-11-07 18:28:24 +0700

    menambahkan enum Navigasi untuk navigasi Formulirku dan Detail

--- app/src/main/java/com/example/questnavigasiui_006/Navigasi.kt
+++ app/src/main/java/com/example/questnavigasiui_006/Navigasi.kt
@@ -1,2 +1,17 @@
 package com.example.questnavigasiui_006
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.questnavigasiui_006.view.FormIsian
+
+enum class Navigasi {
+    Formulirku,
+    Detail
+}
+
