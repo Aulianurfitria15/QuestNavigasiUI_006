cde95a62bb08877f45e844f14ed9ab5671b3db4a
Aulianurfitria15 <aulianurfitria17@gmail.com>
2025-11-07 15:44:04 +0700

    menambahkan tombol Submit dan jarak (Spacer) pada layout form

--- app/src/main/java/com/example/questnavigasiui_006/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasiui_006/view/Formulir.kt
@@ -87,3 +87,13 @@ fun FormIsian(
                 label = { Text(text = "Alamat") },
                 onValueChange = {},
             )
+            Spacer(modifier = Modifier.height(30.dp))
+            Button(
+                modifier = Modifier.fillMaxWidth(1f)
+                    .padding(all = 25.dp),
+                onClick = { OnSubmitBtnClick }
+            ) {
+            }
+        }
+    }
+    }
